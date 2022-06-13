73910059d87aa9972292597ee5027ddc9046ebd0
Kratika Gaonkar <Kratika Gaonkar@DESKTOP-271FC1M>
2022-06-13 12:54:30 +0530

    commit 2

--- SEE/src/Pendrive.java
+++ SEE/src/Pendrive.java
@@ -5,6 +5,10 @@ public class Pendrive {
 	{
 		this.size=size;
 	}
+	void adddata(int file)
+	{
+		size=size+file;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
